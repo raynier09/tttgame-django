9f73cde48869119a8a0305f1792998da0cb65407
Raynier Portus <raynierportus.14@gmail.com>
2021-02-21 14:59:29 +0800

    Providing urls to view methods.

--- game/urls.py
+++ game/urls.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from . import views
 
+# This is the urls.py where you can change the url patterns and provide an URL to views.
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('leaderboard/', views.leaderboard, name='leaderboard'),
+    path('<int:pk>', views.game, name='detail'),
 ]
